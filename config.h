00cc70a9843f51176d893e204d3970f9b3300852
Rishi <rishibhargawa47@gmail.com>
2026-07-25 14:07:44 +0530

    updates: config

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int instant = 1;                     /* -n  option; if 1, select single entry automatically */
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xBF;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 800;                    /* minimum width when centered */
