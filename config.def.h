b869b89d1aee735c4f187b360f07a787dbcf6f40
Rishi <rishibhargawa47@gmail.com>
2026-09-14 23:22:00 +0530

    Modified: Colors.h

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int instant = 1;                     /* -n  option; if 1, select single entry automatically */
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
-static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int fuzzy  = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 2.5f;  /* This is the ratio used in the original calculation */
@@ -32,4 +32,4 @@ static unsigned int columns    = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 0;
